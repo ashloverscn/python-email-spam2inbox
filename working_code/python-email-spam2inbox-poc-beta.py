485fb473da2540cea71e158a76c71df9690bc497
ashloverscn <ashloverscn@gmail.com>
2022-03-26 04:59:50 +0530

    Add files via upload

--- working_code/python-email-spam2inbox-poc-beta.py
+++ working_code/python-email-spam2inbox-poc-beta.py
@@ -13,18 +13,18 @@ try:
 except:
     messages.error(request, 'Request Failed! Unable to connect to Mailbox. Please try again.')
 
-conn.select('"[Gmail]/Spam"') 
+conn.select('"[Gmail]/Spam"')
+
 (retcode, messagess) = conn.uid('search', None, "ALL")
 if retcode == 'OK':
     for num in messagess[0].split():
         typ, data = conn.uid('fetch', num,'(RFC822)')
         msg = email.message_from_bytes((data[0][1]))
-        #MOVE MESSAGE TO ProcessedEmails FOLDER
         result = conn.uid('COPY', num, '"INBOX"')
-        print (result)
         if result[0] == 'OK':
             mov, data = conn.uid('STORE', num , '+FLAGS', '(\Deleted)')
-            print (data)
             conn.expunge()
       
 conn.close()
+
+print ("moving completed")
