ceef2f28dea34d5cb09785925cef5bb260ee0ad1
ashloverscn <ashloverscn@gmail.com>
2022-03-26 05:17:36 +0530

    Add files via upload

--- working_code/python-email-spam2inbox-poc-beta.py
+++ working_code/python-email-spam2inbox-poc-beta.py
@@ -2,6 +2,8 @@ mail_server = 'imap.gmail.com'
 account_id = 'admin@quicksupport.live'
 password = 'tchwsmdjnyfdfyal'
 TLS_port = '993'
+source = '"[Gmail]/Spam"'
+target = '"INBOX"'
 
 import imaplib, email
 
@@ -13,14 +15,14 @@ try:
 except:
     messages.error(request, 'Request Failed! Unable to connect to Mailbox. Please try again.')
 
-conn.select('"[Gmail]/Spam"')
+conn.select(source)
 
 (retcode, messagess) = conn.uid('search', None, "ALL")
 if retcode == 'OK':
     for num in messagess[0].split():
         typ, data = conn.uid('fetch', num,'(RFC822)')
         msg = email.message_from_bytes((data[0][1]))
-        result = conn.uid('COPY', num, '"INBOX"')
+        result = conn.uid('COPY', num, target)
         if result[0] == 'OK':
             mov, data = conn.uid('STORE', num , '+FLAGS', '(\Deleted)')
             conn.expunge()
