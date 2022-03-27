a99633c329799f161d23c45bd65579e1068a1c95
ashloverscn <ashloverscn@gmail.com>
2022-03-28 00:40:00 +0530

    Update example_simple_print_raw_mail.py

--- working_code/example_simple_print_raw_mail.py
+++ working_code/example_simple_print_raw_mail.py
@@ -1,13 +1,25 @@
+########## what i got as folders ##########
+##b'(\\HasNoChildren) "/" "INBOX"'
+##b'(\\HasNoChildren) "/" "Knowledge"'
+##b'(\\HasChildren \\Noselect) "/" "[Gmail]"'
+##b'(\\All \\HasNoChildren) "/" "[Gmail]/All Mail"'
+##b'(\\HasNoChildren \\Trash) "/" "[Gmail]/Bin"'
+##b'(\\Drafts \\HasNoChildren) "/" "[Gmail]/Drafts"'
+##b'(\\HasNoChildren \\Important) "/" "[Gmail]/Important"'
+##b'(\\HasNoChildren \\Sent) "/" "[Gmail]/Sent Mail"'
+##b'(\\HasNoChildren \\Junk) "/" "[Gmail]/Spam"'
+##b'(\\Flagged \\HasNoChildren) "/" "[Gmail]/Starred"'
+
 import imaplib
-import pprint
 
 imap_host = 'imap.gmail.com'
+imap_port = '993'
 imap_user = 'admin@quicksupport.live'
 imap_pass = 'tchwsmdjnyfdfyal'
 folder = "[Gmail]/Spam"
 
 # connect to host using SSL
-imap = imaplib.IMAP4_SSL(imap_host)
+imap = imaplib.IMAP4_SSL(imap_host,imap_port)
 
 ## login to server
 imap.login(imap_user, imap_pass)
@@ -21,18 +33,6 @@ status, messages = imap.select(folder)
 ##for i in imap.list()[1]:
 ##    print(i)
 ##
-
-########## what i got as folders ##########
-##b'(\\HasNoChildren) "/" "INBOX"'
-##b'(\\HasNoChildren) "/" "Knowledge"'
-##b'(\\HasChildren \\Noselect) "/" "[Gmail]"'
-##b'(\\All \\HasNoChildren) "/" "[Gmail]/All Mail"'
-##b'(\\HasNoChildren \\Trash) "/" "[Gmail]/Bin"'
-##b'(\\Drafts \\HasNoChildren) "/" "[Gmail]/Drafts"'
-##b'(\\HasNoChildren \\Important) "/" "[Gmail]/Important"'
-##b'(\\HasNoChildren \\Sent) "/" "[Gmail]/Sent Mail"'
-##b'(\\HasNoChildren \\Junk) "/" "[Gmail]/Spam"'
-##b'(\\Flagged \\HasNoChildren) "/" "[Gmail]/Starred"'
 
 ##tmp, data = imap.search(None, '(FROM "admin@quicksupport.live")')
 tmp, data = imap.search(None, 'ALL')
