a2bf4d93922fc7a18872818592058da0fe675acf
ashloverscn <ashloverscn@gmail.com>
2022-03-28 05:34:37 +0530

    Update reading_emails_in_browser.py

--- working_code/reading_emails_in_browser.py
+++ working_code/reading_emails_in_browser.py
@@ -5,9 +5,12 @@ import webbrowser
 import os
 
 # account credentials
-username = "admin@quicksupport.live"
-password = "tchwsmdjnyfdfyal"
-
+imap_host = 'imap.gmail.com'
+imap_port = '993'
+imap_user = 'admin@quicksupport.live'
+imap_pass = 'tchwsmdjnyfdfyal'
+from_folder = '"[Gmail]/Spam"'
+to_folder = '"INBOX"'
 
 def clean(text):
     # clean text for creating a folder
@@ -17,14 +20,14 @@ def clean(text):
 N = 3
 
 # create an IMAP4 class with SSL, use your email provider's IMAP server
-imap = imaplib.IMAP4_SSL("imap.gmail.com")
+imap = imaplib.IMAP4_SSL(imap_host,imap_port)
 # authenticate
-imap.login(username, password)
+imap.login(imap_user, imap_pass)
 
 # select a mailbox (in this case, the inbox mailbox)
 # use imap.list() to get the list of mailboxes
 ##status, messages = imap.select("INBOX")
-status, messages = imap.select("[Gmail]/Spam")
+status, messages = imap.select(from_folder)
 
 # total number of emails
 messages = int(messages[0])
